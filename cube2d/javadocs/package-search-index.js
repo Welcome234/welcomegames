c8b89fab370d1ce242caf2b419d78c534c3850f7
Welcome234 <68016689+Welcome234@users.noreply.github.com>
2024-10-14 19:40:29 +0100

    Add Javadoc for the Plugin API
    Javadoc added for Plugin API version 1.13.1 of Cube2D.

---
+++ cube2d/javadocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.welcome234.pluginsystem"},{"l":"com.welcome234.pluginsystem.entity"},{"l":"com.welcome234.pluginsystem.events"}];updateSearchResults();
